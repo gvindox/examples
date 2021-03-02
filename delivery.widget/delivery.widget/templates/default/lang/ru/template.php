80ab3020af56b72253cfcae13792b7a5c4c5c54b
gvindox <gvindox@gmail.com>
2021-03-02 16:55:48 +0300

    Delete delivery.widget/delivery.widget directory

--- delivery.widget/delivery.widget/templates/default/lang/ru/template.php
+++
@@ -1,8 +0,0 @@
-<?
-$MESS["LIST_VIEW"] = "Списком";
-$MESS["MAP_VIEW"] = "На карте";
-$MESS["WIDGET_TITLE"] = "Выберите пункт самовывоза";
-$MESS["BOXBERRY_POINT_TITLE"] = "Пункт выдачи посылок BoxBerry";
-$MESS['PVZ_REQUIRED'] = '<span>Необходимо выбрать пункт выдачи Boxberry</span>';
-$MESS['BB_CUSTOM_LINK'] = 'Выбрать пункт выдачи на карте...';
-?>
